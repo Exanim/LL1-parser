7bc68adf4e18c8ce4047215291c020026fa7e02b
Exanim <72408474+Exanim@users.noreply.github.com>
2023-04-26 19:22:49 +0200

    Update Main.java

--- Forditoprogramok/src/Main.java
+++ Forditoprogramok/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         String[] grammarAndWord = new String[2];
 
         // feel free to try out other examples, or your own grammar (must be LL(1) grammar)
-        String grammarFile = "ExampleGrammar3.txt";
+        String grammarFile = "LL1-parser-main/Forditoprogramok/ExampleGrammar3.txt";
         String word = "acdb";
 
         LL1Parser parser = new LL1Parser(grammarFile, word);
